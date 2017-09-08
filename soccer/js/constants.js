cfdc28f3aecd5e115ec9001eeb53fc220ff055fe
ajstroud <ajstroud@users.noreply.github.com>
2017-09-07 19:27:14 -0700

    added in basic js structure

---
+++ soccer/js/constants.js
@@ -0,0 +1,4 @@
+const BALL_MOVEMENT_SKILL_DIVISOR = 40.0;
+const ATTACK_LIMIT_BASE_VALUE = 4.5; // a measure of how far down the field teams need to reach to trigger an attack sequence
+const ATTACK_LIMIT_SKILL_MODIFIER = 0.01; 
+const RESET_TO_DIFFERENCE = 2.0; // a measure of roughly how far the ball gets cleared out
---
+++ soccer/js/sim-screen.js
@@ -0,0 +1,6 @@
+
+class SimScreen {
+  constructor() {
+    
+  }
+}
